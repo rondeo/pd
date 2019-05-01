b5abf731e1b98e03d9aa3f18eeb3d2ede43a2fd2
Никита <nikkraew22@gmail.com>
2019-05-01 22:45:53 +0300

    news

---
+++ src/app/news/news.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import{HttpService} from 'src/app/http.services';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.css']
+})
+export class NewsComponent implements OnInit {
+
+  constructor(private http: HttpClient,private hp:HttpService){}
+  title = 'GGPD';
+  ArrofObjects=[];
+  object={user:"",title:"",description:"",vc:"",enabled:"",date:""};
+
+  ngOnInit(){
+          
+     //this.http.get('http://server.std-763.ist.mospolytech.ru/getNews.php',{responseType:'text'}).subscribe(data=>console.log(data));
+     this.hp.getData("all").subscribe((dat)=>{
+       let count=dat.toString().split(",").length;
+       for(let i=0;i<count;i++){
+          this.ArrofObjects.push({});
+       }
+       for(let i=0;i<count;i++){
+          this.ArrofObjects[i]=dat[i];
+       }
+        
+        console.log(this.ArrofObjects);
+      });
+    }
+}
